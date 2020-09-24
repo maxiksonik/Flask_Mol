8e9b14009824e438a9fbab5715e4a4716c4b5049
maxiksonik <maksimlytsk@gmail.com>
2020-09-25 00:18:40 +0300

    Add migrations and database and some links

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from config import Configuration
-from posts.blueprint import posts
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+db = SQLAlchemy(app)
 
-app.register_blueprint(posts, url_prefix='/blog')
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,7 +1,24 @@
 from flask import Blueprint, render_template
+from models import Post, Tag
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
+
 @posts.route('/')
 def index():
-    return render_template('posts/index.html')
+    posts = Post.query.all()
+    return render_template('posts/index.html', posts=posts)
+
+
+@posts.route('/<slug>')
+def post_detail(slug):
+    post = Post.query.filter(Post.slug == slug).first()
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
+
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
