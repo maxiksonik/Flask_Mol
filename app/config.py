a1f9b6470c4cab8e163adef1109443103ae87c46
maxiksonik <maksimlytsk@gmail.com>
2020-09-26 15:34:10 +0300

    Add edit_post and flask_admin

--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://user:1@localhost/test1'
+    SECRET_KEY = 'somesecret'
