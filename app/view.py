9f8a0feecd6616d75772cfe8a77ec26e51885ab3
maxiksonik <maksimlytsk@gmail.com>
2020-09-29 00:15:01 +0300

    Add custom error handler

--- app/view.py
+++ app/view.py
@@ -4,5 +4,9 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    name = "Vasa"
-    return render_template("index.html", n=name)
+    return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
